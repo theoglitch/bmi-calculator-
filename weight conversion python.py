0169ccf067208fb8f582c8c3ec4936fb6da0d09b
Theo Analysis.00 <141372070+theoglitch@users.noreply.github.com>
2025-11-15 16:52:44 +0200

    Add files via upload

---
+++ weight conversion python.py
@@ -0,0 +1,74 @@
+# Python weight converter 
+# Converts pounds to kilograms and vice versa
+
+
+weight = float(input("Enter your weight:  "))
+unit = input("Kilograms or Pounds? (K or L):  ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs." 
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+else :
+    print(f"{unit}was not valid")
+
+print(f"Your weight is {round(weight)} {unit}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
